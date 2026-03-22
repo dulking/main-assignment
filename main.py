f9f959872edf3deb38a3711811716836266dedab
dulking <culfylord@gmail.com>
2026-03-22 18:58:53 +1300

    The setup of constants for validation and calculations

--- main.py
+++ main.py
@@ -3,3 +3,8 @@ from tkinter import messagebox
 import random
 import math
 from datetime import datetime
+
+# Constants for validation and calculations
+MAX_QUANTITY = 50
+BOX_SIZE = 0
+DATE_FORMAT = "%m/%d/%Y"
