752c2214b21684656e6e4f1256a4e66b0540a3dc
dulking <culfylord@gmail.com>
2026-03-29 15:25:35 +1300

    fixing the errors on the constants for validation and calculation

--- main.py
+++ main.py
@@ -5,9 +5,9 @@ import math
 from datetime import datetime
 
 # Constants for validation and calculations
-MAX_QUANTITY = 50
-BOX_SIZE = 0
-DATE_FORMAT = "%m/%d/%Y"
+MAX_QUANTITY = 500
+BOX_SIZE = 25
+DATE_FORMAT = "%d/%m/%Y"
 
 # Lists used to store program data
 hire_list = {}
